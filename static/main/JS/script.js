f8cba1e4a1d1049139da9c5e48c6d98f0872e112
Mohit Passan <mohitpassan@gmail.com>
2019-08-18 01:25:38 +0530

    Finalised styling of artists page as well as current user page

--- static/main/JS/script.js
+++ static/main/JS/script.js
@@ -1,3 +1,14 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function() {
+    $('.profile-picture').hover(
+        function() {
+            $(this).parent().find('.edit-text').addClass("edit-text-visible");
+        },
+        function() {
+            $(this).parent().find('.edit-text').removeClass("edit-text-visible");
+        }
+    );
+});
