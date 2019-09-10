b5a91e6d77cc93b3d04c11b6064329d209efa2e8
Mohit Passan <mohitpassan@gmail.com>
2019-09-10 14:48:57 +0530

    Static files directory. collectstatic command

--- UrbanArt/settings.py
+++ UrbanArt/settings.py
@@ -129,13 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
